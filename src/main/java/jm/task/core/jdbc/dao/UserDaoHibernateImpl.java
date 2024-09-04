e7fdf3c2f28c6ef89a54bcc07f60e1ae579a148e
Sergey Piunov <piunov92@gmail.com>
2024-09-05 01:18:51 +0300

    init hibernate

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,10 +1,20 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
+import javax.persistence.Query;
 import java.util.List;
 
+import static jm.task.core.jdbc.util.Util.getSessionFactory;
+
 public class UserDaoHibernateImpl implements UserDao {
+    private final SessionFactory sessionFactory = getSessionFactory();
+    private Transaction transaction;
+
     public UserDaoHibernateImpl() {
 
     }
@@ -12,8 +22,42 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
+        Session session = getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
 
+        String sql = "CREATE TABLE IF NOT EXISTS users " +
+                "(id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+                "name VARCHAR(50) NOT NULL, lastName VARCHAR(50) NOT NULL, " +
+                "age TINYINT NOT NULL)";
+
+        Query query = session.createSQLQuery(sql).addEntity(User.class);
+        query.executeUpdate();
+
+        transaction.commit();
+        session.close();
     }
+//    public void createUsersTable() {
+//        try (Session session = sessionFactory.openSession()) {
+//            String sql = "CREATE TABLE IF NOT EXISTS users " +
+//                "(id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+//                "name VARCHAR(50) NOT NULL, lastName VARCHAR(50) NOT NULL, " +
+//                "age TINYINT NOT NULL)";
+//            transaction = session.beginTransaction();
+//            session.createSQLQuery(/*"""
+//                    CREATE TABLE IF NOT EXISTS `pp-1-1-3-4`.`users` (
+//                      `id` INT NOT NULL AUTO_INCREMENT,
+//                      `name` VARCHAR(45) NOT NULL,
+//                      `lastName` VARCHAR(45) NOT NULL,
+//                      `age` TINYINT NOT NULL,
+//                      PRIMARY KEY (`id`));"""*/sql).addEntity(User.class).executeUpdate();
+//            transaction.commit();
+//        } catch (Exception e) {
+//            if (transaction != null) {
+//                transaction.rollback();
+//            }
+//            System.err.println("error creating users table " + e.getMessage());
+//        }
+//    }
 
     @Override
     public void dropUsersTable() {
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,13 +1,25 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.mapping.Property;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
-    private static final String URL = "jdbc:mysql://localhost:3306/pp-1-1-3-4";
+    private static final String URL = "jdbc:mysql://localhost:3306/pp-1-1-3-4?autoReconnect=true&useSSL=false";
     private static final String USER = "root";
     private static final String PASSWORD = "root";
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private static SessionFactory sessionFactory;
+
 
     public static Connection getConnection() {
         Connection connection = null;
@@ -18,4 +30,45 @@ public class Util {
         }
         return connection;
     }
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                //Properties properties = getProperties();
+                //configuration.addAnnotatedClass(User.class);
+                Properties settings = new Properties();
+
+                settings.put(Environment.DRIVER, DRIVER);
+                settings.put(Environment.URL, URL);
+                settings.put(Environment.USER, USER);
+                settings.put(Environment.PASS, PASSWORD);
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+                settings.put(Environment.SHOW_SQL, "true");
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+                settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+
+                configuration.setProperties(settings);
+                configuration.addAnnotatedClass(User.class);
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                System.err.println("Failed to create session factory: " + e);
+            }
+        }
+        return sessionFactory;
+    }
+
+//    private static Properties getProperties() {
+//        Properties properties = new Properties();
+//        properties.setProperty("hibernate.connection.url", URL);
+//        properties.setProperty("hibernate.connection.driver_class", DRIVER);
+//        properties.setProperty("hibernate.connection.username", USER);
+//        properties.setProperty("hibernate.connection.password", PASSWORD);
+//        properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+//        properties.setProperty("hibernate.show_sql", "true");
+//        properties.setProperty("hibernate.connection.provider_class", "org.hibernate.dialect.MySQLDialect");
+//        return properties;
+//    }
 }
