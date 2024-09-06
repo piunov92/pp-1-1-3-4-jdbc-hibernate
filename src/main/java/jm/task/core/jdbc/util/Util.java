3ed06dffbaf06625b0e6de9a6764f8ea149149f0
Sergey Piunov <piunov92@gmail.com>
2024-09-06 17:23:26 +0300

    fix settings Util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -55,6 +55,7 @@ public class Util {
         settings.put(Environment.PASS, PASSWORD);
         settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
         settings.put(Environment.SHOW_SQL, "true");
+        settings.put(Environment.HBM2DDL_AUTO, "update");
         settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
 
         configuration.setProperties(settings);
